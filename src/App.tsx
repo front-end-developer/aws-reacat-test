318111d58a1b77ac3872346d8982801a9a9ba8cd
mark w <mrkwebley@gmail.com>
2021-08-29 15:54:37 +0100

    feat: more use hook examples

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,13 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './App.css';
 import { HooksComponent } from './shared/components/hooks/hooks-component';
 import { LoginComponent } from './shared/components/login-component/login-component';
+import { UseRefHook } from './shared/components/usehooks/use-ref-hook';
+import { UseRefHookAsVar } from './shared/components/usehooks/use-ref-hook-as-var';
+import { UsehooksComponent } from './shared/components/usehooks/usehooks-component';
 
 function App() {
+  const [showUseRefHookAsVar, setShowUseRefHookAsVar] = useState(true);
   return (
     <div className="App">
       <header className="App-header">
@@ -11,7 +15,18 @@ function App() {
       </header>
       <section>
         <LoginComponent />
+        <UsehooksComponent />
         <HooksComponent />
+        <UseRefHook />
+
+        { /* contains var that re-render dos not reset to 0 */}
+        <br /><br />
+        <button onClick={
+          () => setShowUseRefHookAsVar(!showUseRefHookAsVar)}>
+            Toggle
+        </button>
+        {showUseRefHookAsVar && <UseRefHookAsVar />}
+        <br /><br />
       </section>
     </div>
   );
